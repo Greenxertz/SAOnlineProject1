b1c5f3ace66e63a0430e08c3df95a631488dfa40
Greenxertz <Texbar07@gmail.com>
2024-08-29 00:40:29 +0200

    initial Category upload

---
+++ SAOnlineProject1/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SAOnlineProject1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
