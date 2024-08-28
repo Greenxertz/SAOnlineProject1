0349dd80f14d68daa396a0bc73067fab080b3bfa
Greenxertz <Texbar07@gmail.com>
2024-08-29 00:26:44 +0200

    Add project files.

---
+++ SAOnlineProject1/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SAOnlineProject1.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage ="Length cannot go above 30 characters")]
+        public string Name { get; set; }
+
+    }
+}
